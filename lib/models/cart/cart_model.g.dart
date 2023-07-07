59ebac997b05462d720cd07c7610be4a5a22fd60
shinxxxxwon <shinxxxxwon@gmail.com>
2023-07-07 16:34:13 +0900

    commit cart

---
+++ lib/models/cart/cart_model.g.dart
@@ -0,0 +1,16 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'cart_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_CartModel _$$_CartModelFromJson(Map<String, dynamic> json) => _$_CartModel(
+      key: json['key'] as List<dynamic>?,
+    );
+
+Map<String, dynamic> _$$_CartModelToJson(_$_CartModel instance) =>
+    <String, dynamic>{
+      'key': instance.key,
+    };
